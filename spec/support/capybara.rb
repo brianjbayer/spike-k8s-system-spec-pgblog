966452b4784a97a9ca50b72d3bb78bf1465f5088
brianjbayer <brianjbayer@gmail.com>
2021-06-30 21:12:37 -0400

    Refactor RSpec Capybara configuration

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,25 +1,31 @@
 # frozen_string_literal: true
 
-## Register the Environment-Specified Remote Browser ##
-remote_url = ENV['SELENIUM_REMOTE']
-browser_type = ENV['SELENIUM_BROWSER'].to_sym
-Capybara.register_driver :remote_browser do |app|
-  Capybara::Selenium::Driver.new(app,
-                                 browser: :remote,
-                                 url: remote_url,
-                                 desired_capabilities: browser_type)
+def capybara_remote_driver(remote_url, browser_type)
+  remote_browser = :remote_browser
+  Capybara.register_driver remote_browser do |app|
+    Capybara::Selenium::Driver.new(app,
+                                   browser: :remote,
+                                   url: remote_url,
+                                   desired_capabilities: browser_type)
+  end
+  remote_browser
 end
 
-## Configure RSpec Capybara ##
-app_host_port = ENV['APP_HOST_PORT']
+def configure_rspec_capybara(capybara_browser, app_host_port)
 # This is always running locally (even in container) so get local IP address
-app_host = IPSocket.getaddress(Socket.gethostname)
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :remote_browser
+  app_host = IPSocket.getaddress(Socket.gethostname)
+  RSpec.configure do |config|
+    config.before(:each, type: :system) do
+      driven_by capybara_browser
 
-    Capybara.app_host = "http://#{app_host}:#{app_host_port}"
-    Capybara.server_host = app_host
-    Capybara.server_port = app_host_port
+      Capybara.app_host = "http://#{app_host}:#{app_host_port}"
+      Capybara.server_host = app_host
+      Capybara.server_port = app_host_port
+    end
   end
 end
+
+## Configure RSpec System Tests with Environment-Specified Remote Capybara Driver ##
+capybara_browser = capybara_remote_driver(ENV['SELENIUM_REMOTE'], ENV['SELENIUM_BROWSER'].to_sym)
+configure_rspec_capybara(capybara_browser, ENV['APP_HOST_PORT'])
+
